ec3a54f9f5ad676ce9a598bbb06b53b42c45e398
puppetmaster <valentijnbruggeman@gmail.com>
2023-03-07 17:27:15 +0100

    ini groene ogen branch; groene ogen code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) throws Exception {
+        NAO nao = new NAO();
+        nao.verbind("nao.local", 9559);
+        nao.zeg("Hallo Mats! Fijne vakantie gehad?");
+        nao.groeneOgen();
+
+    }
 }
