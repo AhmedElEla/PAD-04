65c205c221a161d26cbd2e1cf213f4e2f120f98f
elaa <ahmed.el.ela@hva.nl>
2023-03-10 17:41:08 +0100

    Main file toegevoegd

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 package src;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Nao naoRobot = new Nao();
         naoRobot.verbind("127.0.0.1", 9559);
