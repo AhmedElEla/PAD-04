937703252cb932767509ade67881d6fc36c88011
elaa <ahmed.el.ela@hva.nl>
2023-03-10 15:59:10 +0100

    Revert "Revert "Naam veranderd""
    This reverts commit caef2e470f9112408920f71f2f3daaf6b2c76849.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import src.RodeOgen;
 
+<<<<<<<< HEAD:src/NAOMain.java
+public class NAOMain {
+========
 public class Main {
+>>>>>>>> parent of 604c471 (Naam veranderd):src/Main.java
 
     public static void main(String[] args) throws Exception {
         RodeOgen nao = new RodeOgen();
