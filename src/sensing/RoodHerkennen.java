2e3ff678a8278401c175b33f2bbeae23339794dd
puppetmaster <valentijnbruggeman@gmail.com>
2023-03-17 15:29:40 +0100

    comment code away

--- src/sensing/RoodHerkennen.java
+++ src/sensing/RoodHerkennen.java
@@ -1,4 +1,14 @@
 package src.sensing;
 
+import com.aldebaran.qi.helper.proxies.ALRedBallDetection;
+
 public class RoodHerkennen {
+    // rodebal detectie
+    public void roodHerkennen() throws Exception{
+        // ALRedBallDetection rodeBal = new ALRedBallDetection(this.application.session());
+        // rodeBal.
+
+
+
+    }
 }
