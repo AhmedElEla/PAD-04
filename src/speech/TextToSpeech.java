3bd3c08439e3f50d51397f83c80a264c68b64767
puppetmaster <valentijnbruggeman@gmail.com>
2023-03-17 21:52:29 +0100

    speech package created, made a TextToSpeech class to call the method "praten"

---
+++ src/speech/TextToSpeech.java
@@ -0,0 +1,17 @@
+package src.speech;
+
+import com.aldebaran.qi.Session;
+import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
+
+public class TextToSpeech {
+    private ALTextToSpeech tts;
+
+    public TextToSpeech(Session session) throws Exception {
+        this.tts = new ALTextToSpeech(session);
+    }
+
+    public void praten(String tekst) throws Exception {
+        this.tts.say(tekst);
+    }
+}
+
