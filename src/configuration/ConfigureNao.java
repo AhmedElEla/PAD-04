eabe22011e915a6b8da6abfe6f6fc1b864e4af9a
puppetmaster <valentijnbruggeman@gmail.com>
2023-03-31 14:32:47 +0200

    changed nao robot name

--- src/configuration/ConfigureNao.java
+++ src/configuration/ConfigureNao.java
@@ -1,7 +1,7 @@
 package src.configuration;
 
 public class ConfigureNao {
-    public static final String HOSTNAME = "johanus.local";
+    public static final String HOSTNAME = "nao.local";
     public static final int PORT = 9559;
 
 
