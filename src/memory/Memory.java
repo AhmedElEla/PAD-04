5159e0b1e4de7ac9b4fbfc467af1f58665a3513e
puppetmaster <valentijnbruggeman@gmail.com>
2023-05-10 23:24:05 +0200

    added method to get redballdata with a List using the updated alMemory and a hashMap

--- src/memory/Memory.java
+++ src/memory/Memory.java
@@ -1,8 +1,12 @@
 package src.memory;
 
+import com.aldebaran.qi.CallError;
 import com.aldebaran.qi.Session;
 import com.aldebaran.qi.helper.EventCallback;
 import com.aldebaran.qi.helper.proxies.ALMemory;
+
+import java.util.List;
+import java.util.Map;
 
 public class Memory {
     private ALMemory memory;
@@ -13,5 +17,17 @@ public class Memory {
 
     public void subscribeToEvent(String event, EventCallback callback) throws Exception {
         memory.subscribeToEvent(event, callback);
+    }
+    public Map<String, Object> getRedBallData() throws CallError, InterruptedException {
+        List<Object> redBallDetected = (List<Object>) memory.getData("redBallDetected");
+
+        Map<String, Object> BallInfo = (Map<String, Object>) redBallDetected.get(1);
+        int x = (int) BallInfo.get("centerX");
+        int y = (int) BallInfo.get("centerY");
+
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+
+        return BallInfo;
     }
 }
