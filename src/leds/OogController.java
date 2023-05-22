c166b36ce8fd0ba33d9c98a266dce24e7b28d10e
puppetmaster <valentijnbruggeman@gmail.com>
2023-05-22 16:24:39 +0200

    ogen LEDS met RGB waarde besturen

--- src/leds/OogController.java
+++ src/leds/OogController.java
@@ -1,5 +1,6 @@
 package src.leds;
 
+import com.aldebaran.qi.CallError;
 import com.aldebaran.qi.Session;
 import com.aldebaran.qi.helper.proxies.ALLeds;
 
@@ -10,5 +11,8 @@ public class OogController {
     }
     public void bepaalOogKleur(String color, float duration) throws Exception {
         this.ogenLeds.fadeRGB ("FaceLeds", color, duration);
+    }
+    public void bepaalRGBWaarde(String name, Integer rgb, Float duration) throws CallError, InterruptedException {
+        this.ogenLeds.fadeRGB(name, rgb, duration);
     }
 }
