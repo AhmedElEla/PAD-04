f2ce9fdcce855e88f4d4bb90e23df363a2f46fbb
puppetmaster <valentijnbruggeman@gmail.com>
2023-05-24 12:28:02 +0200

    proper main for buttons and redBallDetection

--- src/Main.java
+++ src/Main.java
@@ -13,68 +13,15 @@ public class Main {
         Nao naoTyrone = new Nao();
         naoTyrone.verbind();
         naoTyrone.praten("Verbonden");
+        naoTyrone.postureInput("Standinit", 1f);
 
         naoTyrone.detectRedBall();
-        naoTyrone.postureInput("Standinit", 1f);
+
         naoTyrone.setBackgroundmovement(true);
 
-        EventCallback frontTouchEventCallback = o -> {
-            float touch = (float)o;
-            float touchThreshold = 0.5f;
-            if (touch >= touchThreshold) {
-                naoTyrone.setBackgroundmovement(false);
-                //System.out.println("Voor knop gedrukt");
-                try {
-                    // Do something when front button is pressed
-                    naoTyrone.postureInput("StandInit", 0.5f);
-                    naoTyrone.animateSpeech("^startTag(Hey_1) Hallo, ^wait(Hey_1) ^start(animations/Stand/Gestures/Explain_10) mijn naam is Tyrone. Ik ben gemaakt om jullie te helpen bewegen! 1 van mijn spel modes is gemaakt zodat jullie mij na kunnen doen. Klik de knop op het midden van mijn hoofd om te beginnen!");
-                    Thread.sleep(500);
-                } catch (Exception e) {
-                    System.out.println(e);
-                    throw new RuntimeException(e);
-                }
-            }
-        };
-
-        EventCallback middleTouchEventCallback = o -> {
-            float touch = (float)o;
-            float touchThreshold = 0.5f;
-            if (touch >= touchThreshold) {
-                naoTyrone.setBackgroundmovement(false);
-                //System.out.println("Midden knop gedrukt");
-                try {
-                    // Do something when middle button is pressed
-                    naoTyrone.animateSpeech("Ik leg nu uit hoe het spel werkt. ^start(animations/Stand/Gestures/YouKnowWhat_1) Dit spel heet simon says! Doe mijn bewegingen zo goed mogelijk na en probeer zoveel mogelijk plezier te hebben bij het spelen");
-                    Thread.sleep(500);
-                    naoTyrone.simonSays();
-                    naoTyrone.postureInput("Crouch", 0.5f);
-                } catch (Exception e) {
-                    System.out.println(e);
-                    throw new RuntimeException(e);
-                }
-            }
-        };
-
-        EventCallback rearTouchEventCallback = o -> {
-            float touch = (float)o;
-            float touchThreshold = 0.5f;
-            if (touch >= touchThreshold) {
-                naoTyrone.setBackgroundmovement(false);
-                //System.out.println("Achterste knop gedrukt");
-                try {
-                    naoTyrone.postureInput("StandInit", 0.5f);
-                    naoTyrone.animateSpeech("  ^start(animations/Stand/Gestures/Enthusiastic_4) U heeft de laatste knop ingedrukt!! ^wait(animations/Stand/Gestures/Enthusiastic_4) ^start(animations/Stand/BodyTalk/BodyTalk_10) Hier komt de show en dans van kinderen voor kinderen! In de laatste sprint zien jullie het resultaat");
-                    Thread.sleep(500);
-                } catch (Exception e) {
-                    System.out.println(e);
-                    throw new RuntimeException(e);
-                }
-            }
-        };
-
-        naoTyrone.touchButton("Front", frontTouchEventCallback);
-        naoTyrone.touchButton("Middle", middleTouchEventCallback);
-        naoTyrone.touchButton("Rear", rearTouchEventCallback);
+        naoTyrone.touchButton("Front");
+        naoTyrone.touchButton("Middle");
+        naoTyrone.touchButton("Rear");
 
         naoTyrone.doWhile(1000, 100);
     }
