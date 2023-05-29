65db5f047d386657bbbf5443d2494459072f7539
elaa <ahmed.el.ela@hva.nl>
2023-05-29 22:29:50 +0200

    Added AudioDevice in the Audioplayer class and Changed the class into AudioController

--- src/audio/AudioController.java
+++ src/audio/AudioController.java
@@ -2,15 +2,21 @@ package src.audio;
 
 import com.aldebaran.qi.CallError;
 import com.aldebaran.qi.Session;
+import com.aldebaran.qi.helper.proxies.ALAudioDevice;
 import com.aldebaran.qi.helper.proxies.ALAudioPlayer;
 
-public class AudioPlayer {
+public class AudioController {
     private ALAudioPlayer audioPlayer;
-    public AudioPlayer(Session session) throws Exception {
+    private ALAudioDevice audioDevice;
+    public AudioController(Session session) throws Exception {
         this.audioPlayer = new ALAudioPlayer(session);
+        this.audioDevice = new ALAudioDevice(session);
     }
     public void playSFX(String filename) throws CallError, InterruptedException {
         audioPlayer.setMasterVolume(0.5f);
         audioPlayer.playFile(filename);
+    }
+    public void setOutputVolume(int volume) throws Exception {
+        audioDevice.setOutputVolume(volume);
     }
 }
