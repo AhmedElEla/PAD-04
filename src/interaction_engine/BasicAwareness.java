92b73b2a359ac08b7e7150949a6459197836148e
elaa <ahmed.el.ela@hva.nl>
2023-06-01 16:48:46 +0200

    Put comments for the vision package

--- src/interaction_engine/BasicAwareness.java
+++ src/interaction_engine/BasicAwareness.java
@@ -5,20 +5,21 @@ import com.aldebaran.qi.Session;
 import com.aldebaran.qi.helper.proxies.ALBasicAwareness;
 
 public class BasicAwareness {
-
+    // Acces modifier for ALBasicAwareness
     private ALBasicAwareness basicAwareness;
-
+    // Constructor that initializes ALBasicAwareness
     public BasicAwareness (Session session) throws Exception {
         basicAwareness = new ALBasicAwareness(session);
     }
-
+    // Method for the engagement mode of the NAO using BasicAwareness
     public void engagementMode (String modename) throws CallError, InterruptedException {
         basicAwareness.setEngagementMode(modename);
     }
+    // Method for the head tracker of the NAO using BasicAwareness
     public void headTracker (String trackingmode) throws CallError, InterruptedException {
         basicAwareness.setTrackingMode(trackingmode);
     }
-
+    // Method for the stimulus mode of the NAO using BasicAwareness
     public void stimulusMode (String stimtype, boolean enable) throws CallError, InterruptedException {
         basicAwareness.setStimulusDetectionEnabled(stimtype, enable);
     }
