db2e8811abe5a31effb05611c1cfdc420a930d88
elaa <ahmed.el.ela@hva.nl>
2023-06-01 16:48:13 +0200

    Put comments for the speech package

--- src/speech/AnimatedSpeech.java
+++ src/speech/AnimatedSpeech.java
@@ -5,10 +5,13 @@ import com.aldebaran.qi.Session;
 import com.aldebaran.qi.helper.proxies.ALAnimatedSpeech;
 
 public class AnimatedSpeech {
+    // Acces modifier for ALAnimatedSpeech
     private ALAnimatedSpeech animatedSpeech;
+    // Constructor that initializes ALAnimatedSpeech
     public AnimatedSpeech(Session session) throws Exception {
         this.animatedSpeech = new ALAnimatedSpeech(session);
     }
+    // Method that makes the NAO moves while he talks
     public void animatedText(String text) throws CallError, InterruptedException {
         this.animatedSpeech.say(text);
     }
--- src/speech/TextToSpeech.java
+++ src/speech/TextToSpeech.java
@@ -5,13 +5,17 @@ import com.aldebaran.qi.Session;
 import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
 
 public class TextToSpeech {
+    // Acces modifier for ALTextToSpeech
     private ALTextToSpeech tts;
+    // Constructor that initializes ALTextToSpeech
     public TextToSpeech(Session session) throws Exception {
         this.tts = new ALTextToSpeech(session);
     }
+    // Method that makes the NAO say what we desire
     public void talk(String tekst) throws Exception {
         this.tts.say(tekst);
     }
+    // Method that changes the talking language of the NAO
     public void Language(String language) throws CallError, InterruptedException {
         this.tts.setLanguage(language);
     }
