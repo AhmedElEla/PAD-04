cd3147e76f93a1c2036f1c046e7b61bd1e0e2c4d
elaa <ahmed.el.ela@hva.nl>
2023-06-01 16:49:14 +0200

    Put comments for the core package

--- src/core/BehaviourController.java
+++ src/core/BehaviourController.java
@@ -5,10 +5,13 @@ import com.aldebaran.qi.Session;
 import com.aldebaran.qi.helper.proxies.ALBehaviorManager;
 
 public class BehaviourController {
+    // Created an acces modifier for ALBehaviourManager
     private ALBehaviorManager robotBehaviour;
+    // Created a constructor that initializes ALBehaviourManager
     public BehaviourController(Session session) throws Exception {
         robotBehaviour = new ALBehaviorManager(session);
     }
+    // Created a method that is designed so we can import behaviours from Choreographe
     public void behaviour(String behavior) throws CallError, InterruptedException {
         this.robotBehaviour.startBehavior(behavior);
     }
