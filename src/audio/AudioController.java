96dbf1af6b909acb901c2d63f94164b10f2a89e3
elaa <ahmed.el.ela@hva.nl>
2023-06-01 16:48:27 +0200

    Put comments for the audio package

--- src/audio/AudioController.java
+++ src/audio/AudioController.java
@@ -9,18 +9,17 @@ public class AudioController {
     // Created an acces modifier for audioPlayer and for audioDevice
     private ALAudioPlayer audioPlayer;
     private ALAudioDevice audioDevice;
-
     // Created a constructor called AudioController that initializes two instance variables
     public AudioController(Session session) throws Exception {
         this.audioPlayer = new ALAudioPlayer(session);
         this.audioDevice = new ALAudioDevice(session);
     }
-
     // Created a method called playSFX for added sound effects and a method setOutputVolume so i can control how loud the nao is
     public void playSFX(String filename) throws CallError, InterruptedException {
         audioPlayer.setMasterVolume(0.5f);
         audioPlayer.playFile(filename);
     }
+    // Created a method that is used to change the NAO volume
     public void setOutputVolume(int volume) throws Exception {
         audioDevice.setOutputVolume(volume);
     }
